dc595fae368b72f310c9a10f4752b26a3af96b55
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-19 13:23:11 +0300

    Update notifications.lua

--- vorp_core/client/notifications.lua
+++ vorp_core/client/notifications.lua
@@ -1,3 +1,4 @@
+
 -- NOTIFICATIONS
 RegisterNetEvent('vorp:NotifyLeft')
 RegisterNetEvent('vorp:Tip')
@@ -17,31 +18,31 @@ RegisterNetEvent('vorp:LeftRank')
 
 
 AddEventHandler('vorp:NotifyLeft', function(firsttext, secondtext, dict, icon, duration, color)
-    VorpNotification:NotifyLeft(tostring(firsttext), tostring(secondtext), tostring(dict), tostring(icon), tonumber(duration), (tostring(color) or "COLOR_WHITE"))
+    exports.tpz_core:getCoreAPI().NotifyLeft(tostring(firsttext), tostring(secondtext), tostring(dict), tostring(icon), tonumber(duration), (tostring(color) or "COLOR_WHITE"))
 end)
 
 AddEventHandler('vorp:Tip', function(text, duration)
-    VorpNotification:NotifyTip(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyTip(tostring(text), tonumber(duration))
 end)
 
 AddEventHandler('vorp:NotifyTop', function(text, location, duration)
-    VorpNotification:NotifyTop(tostring(text), tostring(location), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyTop(tostring(text), tostring(location), tonumber(duration))
 end)
 
 AddEventHandler('vorp:TipRight', function(text, duration)
-    VorpNotification:NotifyRightTip(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyRightTip(tostring(text), tonumber(duration))
 end)
 
 AddEventHandler('vorp:TipBottom', function(text, duration)
-    VorpNotification:NotifyObjective(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyObjective(tostring(text), tonumber(duration))
 end)
 
 AddEventHandler('vorp:ShowTopNotification', function(tittle, subtitle, duration)
-    VorpNotification:NotifySimpleTop(tostring(tittle), tostring(subtitle), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifySimpleTop(tostring(tittle), tostring(subtitle), tonumber(duration))
 end)
 
 AddEventHandler('vorp:ShowAdvancedRightNotification', function(text, dict, icon, text_color, duration, quality)
-    VorpNotification:NotifyAvanced(tostring(text), tostring(dict), tostring(icon),
+    exports.tpz_core:getCoreAPI().NotifyAvanced(tostring(text), tostring(dict), tostring(icon),
         tostring(text_color), tonumber(duration), quality)
 end)
 
@@ -50,30 +51,30 @@ AddEventHandler('vorp:ShowBasicTopNotification', function(text, duration)
 end)
 
 AddEventHandler('vorp:ShowSimpleCenterText', function(text, duration)
-    VorpNotification:NotifyCenter(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyCenter(tostring(text), tonumber(duration))
 end)
 
 AddEventHandler('vorp:ShowBottomRight', function(text, duration)
-    VorpNotification:NotifyBottomRight(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyBottomRight(tostring(text), tonumber(duration))
 end)
 
 AddEventHandler('vorp:failmissioNotifY', function(title, subtitle, duration)
-    VorpNotification:NotifyFail(tostring(title), tostring(subtitle), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyFail(tostring(title), tostring(subtitle), tonumber(duration))
 end)
 
 AddEventHandler('vorp:deadplayerNotifY', function(title, audioRef, audioName, duration)
-    VorpNotification:NotifyDead(tostring(title), tostring(audioRef), tostring(audioName), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyDead(tostring(title), tostring(audioRef), tostring(audioName), tonumber(duration))
 end)
 
 AddEventHandler('vorp:updatemissioNotify', function(utitle, umsg, duration)
-    VorpNotification:NotifyUpdate(tostring(utitle), tostring(umsg), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyUpdate(tostring(utitle), tostring(umsg), tonumber(duration))
 end)
 
 AddEventHandler('vorp:warningNotify', function(title, msg, audioRef, audioName, duration)
-    VorpNotification:NotifyWarning(tostring(title), tostring(msg), tostring(audioRef), tostring(audioName),
+    exports.tpz_core:getCoreAPI().NotifyWarning(tostring(title), tostring(msg), tostring(audioRef), tostring(audioName),
         tonumber(duration))
 end)
 
 AddEventHandler('vorp:LeftRank', function(title, subtitle, dict, icon, duration, color)
-    VorpNotification:NotifyLeftRank(tostring(title), tostring(subtitle), tostring(dict), tostring(icon), tonumber(duration), (tostring(color)))
+    exports.tpz_core:getCoreAPI().NotifyLeftRank(tostring(title), tostring(subtitle), tostring(dict), tostring(icon), tonumber(duration), (tostring(color)))
 end)
