ed9b398b09a22e74a4ca9feb271176ceedf77a0b
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-19 10:10:27 +0300

    Add files via upload

---
+++ vorp_core/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+lua54 'yes'
+
+name 'vorp core'
+description 'A Tool to build your RedM server and scripts'
+author 'VORP' -- converted from the original C# vorp core by goncalobsccosta#9041
+repository 'https://github.com/VORPCORE/vorp-core-lua'
+
+
+client_scripts {
+    'client/ref/*.lua',
+    'client/exports/*.lua',
+    'client/*.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*lua',
+}
