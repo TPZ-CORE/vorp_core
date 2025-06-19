dc1f782c15448ce532ea6814a49986066be825e2
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-19 13:27:03 +0300

    Update clientapi.lua

--- vorp_core/client/clientapi.lua
+++ vorp_core/client/clientapi.lua
@@ -6,11 +6,11 @@ CoreFunctions.RpcCall = function(name, callback, ...)
 end
 
 CoreFunctions.instancePlayers = function(set)
-    TriggerServerEvent("vorp_core:instanceplayers", set)
+    TriggerServerEvent("tpz_core:instanceplayers", set)
 end
 
 CoreFunctions.AddWebhook = function(title, webhook, description, color, name, logo, footerlogo, avatar)
-    TriggerServerEvent('vorp_core:addWebhook', title, webhook, description, color, name, logo, footerlogo, avatar)
+    --TriggerServerEvent('vorp_core:addWebhook', title, webhook, description, color, name, logo, footerlogo, avatar)
 end
 
 CoreFunctions.NotifyTip = function(text, duration)
@@ -22,56 +22,55 @@ CoreFunctions.NotifyLeft = function(title, subtitle, dict, icon, duration, color
 end
 
 CoreFunctions.NotifyRightTip = function(text, duration)
-    VorpNotification:NotifyRightTip(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyRightTip(tostring(text), tonumber(duration))
 end
 
 CoreFunctions.NotifyObjective = function(text, duration)
-    TriggerEvent('vorp:TipBottom', text, duration) -- listner
-    -- VorpNotification:NotifyObjective(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyObjective(tostring(text), tonumber(duration))
 end
 
 CoreFunctions.NotifyTop = function(text, location, duration)
-    VorpNotification:NotifyTop(tostring(text), tostring(location), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyTop(tostring(text), tostring(location), tonumber(duration))
 end
 
 CoreFunctions.NotifySimpleTop = function(text, subtitle, duration)
-    VorpNotification:NotifySimpleTop(tostring(text), tostring(subtitle), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifySimpleTop(tostring(text), tostring(subtitle), tonumber(duration))
 end
 
 CoreFunctions.NotifyAvanced = function(text, dict, icon, text_color, duration, quality, showquality)
-    VorpNotification:NotifyAvanced(tostring(text), tostring(dict), tostring(icon), tostring(text_color), tonumber(duration), quality, showquality)
+    exports.tpz_core:getCoreAPI().NotifyAvanced(tostring(text), tostring(dict), tostring(icon), tostring(text_color), tonumber(duration), quality, showquality)
 end
 
 CoreFunctions.NotifyBasicTop = function(text, duration)
-    VorpNotification:NotifyBasicTop(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyBasicTop(tostring(text), tonumber(duration))
 end
 
 CoreFunctions.NotifyCenter = function(text, duration, text_color)
-    VorpNotification:NotifyCenter(tostring(text), tonumber(duration), tostring(text_color))
+    exports.tpz_core:getCoreAPI().NotifyCenter(tostring(text), tonumber(duration), tostring(text_color))
 end
 
 CoreFunctions.NotifyBottomRight = function(text, duration)
-    VorpNotification:NotifyBottomRight(tostring(text), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyBottomRight(tostring(text), tonumber(duration))
 end
 
 CoreFunctions.NotifyFail = function(text, subtitle, duration)
-    VorpNotification:NotifyFail(tostring(text), tostring(subtitle), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyFail(tostring(text), tostring(subtitle), tonumber(duration))
 end
 
 CoreFunctions.NotifyDead = function(title, audioRef, audioName, duration)
-    VorpNotification:NotifyDead(tostring(title), tostring(audioRef), tostring(audioName), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyDead(tostring(title), tostring(audioRef), tostring(audioName), tonumber(duration))
 end
 
 CoreFunctions.NotifyUpdate = function(title, subtitle, duration)
-    VorpNotification:NotifyUpdate(tostring(title), tostring(subtitle), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyUpdate(tostring(title), tostring(subtitle), tonumber(duration))
 end
 
 CoreFunctions.NotifyWarning = function(title, msg, audioRef, audioName, duration)
-    VorpNotification:NotifyWarning(tostring(title), tostring(msg), tostring(audioRef), tostring(audioName), tonumber(duration))
+    exports.tpz_core:getCoreAPI().NotifyWarning(tostring(title), tostring(msg), tostring(audioRef), tostring(audioName), tonumber(duration))
 end
 
 CoreFunctions.NotifyLeftRank = function(title, subtitle, dict, icon, duration, color)
-    VorpNotification:NotifyLeftRank(tostring(title), tostring(subtitle), tostring(dict), tostring(icon), tonumber(duration), tostring(color or "COLOR_WHITE"))
+    exports.tpz_core:getCoreAPI().NotifyLeftRank(tostring(title), tostring(subtitle), tostring(dict), tostring(icon), tonumber(duration), tostring(color or "COLOR_WHITE"))
 end
 
 
